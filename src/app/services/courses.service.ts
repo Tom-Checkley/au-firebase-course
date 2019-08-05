2ccb7298a226938c183d2e2bdcbe5e8b4d7438fb
Tom Checkley <Tom-Checkley@users.noreply.github.com>
2019-08-05 22:08:21 +0100

    refactored out loadAllCourses function to courses service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map, first } from 'rxjs/operators';
+import { Course } from '../model/course';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.db.collection('courses')
+      .snapshotChanges()
+      .pipe(
+        map(snaps => {
+          return snaps.map(snap => {
+            return <Course> {
+              id: snap.payload.doc.id,
+              ...snap.payload.doc.data()
+            };
+          });
+        }),
+        first()
+      );
+  }
 }
