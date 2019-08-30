02e5e72ef00461e851095a1738401bd7d61d05b0
Tom Checkley <tom.checkley@element78.co.uk>
2019-08-30 17:01:36 +0100

    added login & logout and goggle avatar image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
+  pictureUrl$: Observable<string>;
+
+  constructor(private afAuth: AngularFireAuth) {}
+
+  ngOnInit(): void {
+    this.afAuth.authState.subscribe(user => console.log(user));
+
+    this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+    this.isLoggedOut$ = this.afAuth.authState.pipe(map(loggedIn => !loggedIn));
+
+    this.pictureUrl$ = this.afAuth.authState.pipe(map(user => user ? user.photoURL : null));
+  }
 
 
-    logout() {
-
-    }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import * as firebase from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,11 +10,11 @@ import { AngularFireAuth } from '@angular/fire/auth';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
   ui: firebaseui.auth.AuthUI;
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router, private ngZone: NgZone) { }
 
   ngOnInit() {
 
@@ -33,8 +34,12 @@ export class LoginComponent implements OnInit {
 
   }
 
-  onLoginSuccessful() {
+  ngOnDestroy() {
+    this.ui.delete();
+  }
 
+  onLoginSuccessful(result) {
+    this.ngZone.run(() => this.router.navigateByUrl('/courses'));
   }
 
 }
